a1f73415b5b7b16846a14a6ec005f24e955bb095
Charlie Miller <cmille24@binghamton.edu>
2017-04-26 03:11:41 +0000

    adding decryption implementation

--- decrypt2.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_POST['emessage']) ){
-  $message =  $_POST['emessage'];
-  echo "This page will respond with a hash of the decrypted message. You just sent ".$message;
-}
-else{
-  echo "You didn't send anything.";
-}
-?>
